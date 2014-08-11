d2618507acbc1db3151d17ba1811394b144fcf2a
ASementsov <ASementsov@ASementsov.ibs.root.ibscorp.ru>
2014-08-11 16:22:08 +0400

    add security

---
+++ src/test/java/org/mesol/spmes/EquipmentTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014 Mes Solutions.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mesol.spmes;
+
+import javax.transaction.Transactional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mesol.spmes.config.PersistenceJPAConfig;
+import org.mesol.spmes.config.RootConfiguration;
+import org.mesol.spmes.config.WebMvcConfiguration;
+import org.mesol.spmes.config.WebMvcSecurityConfig;
+import org.mesol.spmes.model.security.User;
+import org.mesol.spmes.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.Assert;
+
+/**
+ *
+ * @author ASementsov
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration (classes = {
+    RootConfiguration.class,
+    PersistenceJPAConfig.class,
+    WebMvcConfiguration.class,
+    WebMvcSecurityConfig.class
+})
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+@WebAppConfiguration
+public class EquipmentTest {
+
+    @Configuration
+	@EnableJpaRepositories
+	@EnableJpaAuditing
+	static class Config {
+
+	}
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Test
+    public void test() {
+        Assert.notNull(userRepo, "User repository must not be null");
+        User user = userRepo.findByName("admin");
+        Assert.notNull(user);
+    }
+}
