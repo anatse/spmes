0dc84b91f919908854b7f2bde95f47ca60305bc5
ASementsov <ASementsov@ASementsov.ibs.root.ibscorp.ru>
2014-10-17 13:52:06 +0400

    Fix issues

--- src/main/webapp/app/view/user/List.js
+++ src/main/webapp/app/view/user/List.js
@@ -10,13 +10,6 @@ Ext.define('NeoMes.view.user.List', {
 
     initComponent: function () {
         this.store = Ext.create('NeoMes.store.Users');
-//        var model = store.getModel();
-//        var columns = [];
-//
-//        Ext.each (model.getFields(), function (field) {
-//            columns.push ();
-//        });
-
         this.columns = [
             {header: 'Name', dataIndex: 'username', flex:1},
             {header: 'Email', dataIndex: 'email', flex:1}
