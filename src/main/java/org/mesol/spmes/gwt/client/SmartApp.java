a8bb9e5aa2908839119d38cdefbfffaa3007ec67
ASementsov <ASementsov@ASementsov.ibs.root.ibscorp.ru>
2014-12-28 15:30:43 +0400

    656

---
+++ src/main/java/org/mesol/spmes/gwt/client/SmartApp.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 Mes Solutions.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mesol.spmes.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * 
+ * @version 1.0.0
+ * @author ASementsov
+ */
+public class SmartApp implements EntryPoint
+{
+    @Override
+    public void onModuleLoad() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
